7562d54eabf0b1f73b484774b9fc7961ec657504
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-14 17:16:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,24 +13,3 @@ class Shoe
 
   
 end
-
-
-
-
-
- #attr_accessor :author, :page_count, :genre
-#     attr_reader :title
-# 
-#  def initialize(title)
-#    @title = title
-#  end
-# 
-#   def turn_page
-#    puts "Flipping the page...wow, you read fast!"
-#  end
-#end
-
-#book = Book.new("Some Title")
-#book.turn_page
-
-
