a9f0b04ca52c9c2cff25fce3136613f2f8d2e4f8
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-14 15:43:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,41 +1,21 @@
 class Book
   
-  def initialize (title)
+    attr_accessor :author, :page_count, :genre
+ 
+  def initialize(title)
     @title = title
   end
-  
+ 
   def title
     @title
   end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
  
-  def page_count
-    @page_count
-  end
-  
-   def genre=(genre)
-    @genre = genre
-  end
- 
-  def genre
-    @genre
-  end
-  
-   def turn_page
+  def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
+ 
+end
+
 end
 
 book = Book.new("Some Title")
